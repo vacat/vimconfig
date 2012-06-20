7b7944dfb6c5f9a5acf31c6c048a64a27b372c8e
javen.c <javencao@gmail.com>
2012-06-20 08:51:01 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+OS="unknown"
+
+if [ $OSTYPE == linux-gnu ]; then
+    OS="linux" 
+elif [ $OSTYPE == darwin11 ]; then
+    OS="mac" 
+elif [ $OSTYPE == cygwin ]; then
+    OS="cygwin"
+elif [ $OSTYPE == win32 ]; then
+    OS="win32"
+elif [ $OSTYPE == freebsd ]; then
+    OS="bsd"
+else
+    OS="unknown"
+fi
+
+echo $OS
+
+if [ "$OS" == mac ]; then
+    CURDIR=$PWD
+else
+    CURDIR=`dirname $(readlink -f $0)`
+fi
+
+echo $CURDIR
+
+VIMRC=$HOME/.vimrc
+VIMDIR=$HOME/.vim
+
+echo $VIMRC
+echo $VIMDIR
+
+# symlink .vimrc
+if [ -f $VIMRC ]; then
+    mv "$VIMRC" "$VIMRC.bak"
+fi
+ln -s "$CURDIR/_vimrc" "$VIMRC"
+
+# symlink .vim
+if [ -d $VIMDIR ]; then
+    mv "$VIMDIR" "$VIMDIR.bak"
+fi
+ln -s "$CURDIR" "$VIMDIR"
